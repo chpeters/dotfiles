067628476320acbdf9fd13a3800b7175b40a4380
Charlie Peters <chpeters13@gmail.com>
2026-05-02 13:20:28 -0700

    Add Neovim review workflow

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,13 +1,32 @@
 local M = {}
 
+local function set_diff_overlay_highlights()
+  local red = "#ff5f5f"
+
+  vim.api.nvim_set_hl(0, "MiniDiffOverDelete", { fg = red })
+  vim.api.nvim_set_hl(0, "MiniDiffOverChange", { fg = red })
+  vim.api.nvim_set_hl(0, "MiniDiffOverChangeBuf", { fg = red })
+end
+
 function M.setup(pack)
   pack.start({
+    { src = pack.gh("nvim-mini/mini.diff"), name = "mini.diff" },
     { src = pack.gh("nvim-mini/mini.icons"), name = "mini.icons" },
     { src = pack.gh("nvim-tree/nvim-web-devicons"), name = "nvim-web-devicons" },
   })
 
+  require("mini.diff").setup()
+  set_diff_overlay_highlights()
   require("mini.icons").setup()
   require("nvim-web-devicons").setup({})
+
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = set_diff_overlay_highlights,
+  })
+
+  vim.keymap.set("n", "<leader>do", function()
+    require("mini.diff").toggle_overlay()
+  end, { desc = "Toggle diff overlay" })
 end
 
 return M
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -206,7 +206,20 @@ local function set_keymaps(snacks, pack)
     { "<leader>gb", function() snacks.picker.git_branches() end, desc = "Git Branches" },
     { "<leader>gl", function() snacks.picker.git_log() end, desc = "Git Log" },
     { "<leader>gL", function() snacks.picker.git_log_line() end, desc = "Git Log Line" },
-    { "<leader>gs", function() snacks.picker.git_status() end, desc = "Git Status" },
+    { "<leader>gs", function()
+      snacks.picker.git_status({
+        auto_close = false,
+        focus = "list",
+        jump = {
+          close = false,
+          reuse_win = true,
+        },
+        layout = {
+          preset = "sidebar",
+          preview = false,
+        },
+      })
+    end, desc = "Git Status" },
     { "<leader>gS", function() snacks.picker.git_stash() end, desc = "Git Stash" },
     { "<leader>gd", function() snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
     { "<leader>gf", function() snacks.picker.git_log_file() end, desc = "Git Log File" },
